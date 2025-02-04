de02be081e7eae2e49847229ebacccf20ca3e6d0
Денис Богомолов <31684886+den3011den@users.noreply.github.com>
2025-02-05 03:08:41 +0700

    Добавлены сущности Fact и FactComment

---
+++ Facts_DataAccess/FactsDB/BaseEntity.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Facts_DataAccess.FactsDB
+{
+    /// <summary>
+    /// Базовый класс сущностей
+    /// </summary>
+    public class BaseEntity
+    {
+        /// <summary>
+        /// ИД сущности
+        /// </summary>
+        [Key]
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Comment("ИД записи")]
+        public int Id { get; set; }
+    }
+}
---
+++ Facts_DataAccess/FactsDB/FactComment.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Facts_DataAccess.FactsDB
+{
+
+    /// <summary>
+    /// Комментарий к факту выдачи/возврата экземпляров книг
+    /// </summary>
+    [Table("FactComments")]
+    [Index("FactId", "BookInstanceId" IsUnique = true, Name = "FactIdBookInstanceIdUnique")]
+    [Comment("Таблица комментариев фактов выдачи/возврата экземпляров книг")]
+    public class FactComment : BaseEntity
+    {
+        [Required]
+        [Comment("ИД факта")]
+        public int FactId { get; set; }
+
+        /// <summary>
+        /// Факт выдачи/возврата экземпляра книги
+        /// </summary>
+        public Fact Fact { get; set; }
+
+        /// <summary>
+        /// ИД Экземпляра книги
+        /// </summary>
+        [Required]
+        [Comment("ИД экземпляра книги")]
+        public int BookInstanceId { get; set; }
+
+        /// <summary>
+        /// Комментарий
+        /// </summary>
+        [Required]
+        [MaxLength(300)]
+        [MinLength(3)]
+        [Comment("Комментарий")]
+        public string Comment { get; set; }
+    }
+}
