977fea9d70d6be722ebde451bbc9a91427f2d64a
Денис Богомолов <31684886+den3011den@users.noreply.github.com>
2025-03-04 02:28:25 +0700

    Добавлен маппинг сущностей для States

---
+++ Facts_Business/Mapper/MappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Facts_Domain.FactsDB;
+using Facts_Models.FactsModels.State;
+
+namespace Facts_Business.Mapper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<State, StateItemResponse>();
+            CreateMap<State, StateItemCreateRequest>();
+            CreateMap<State, StateItemUpdateRequest>();
+
+        }
+    }
+}
